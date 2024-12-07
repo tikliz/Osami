17ee472cdbe8cae60d0858a1c99ac146fd9f7455
tikliz <tik8lizz@gmail.com>
2024-12-07 10:53:19 -0300

    fixed var infer

--- Main.gd
+++ Main.gd
@@ -64,16 +64,16 @@ func distance(point1: Vector2, point2: Vector2) -> float:
 func calculate_score_straight_line(line_data: BrushStroke, target_line: BrushStroke) -> float:
 	
 	# check if the line is completed before calculating anything
-	var points = line_data.data
+	var points := line_data.data
 	# calculate sum of all points to target distances multiplicated by the weight
-	var sum = 0.0
+	var sum := 0.0
 	var weight_sum = 0.0
 	for i in range(points.size() - 1):
 		#var point_data = line_data[i]
 		#var point_data_next = line_data[i + 1]
-		var point_time_delta = points[i + 1].timestamp - points[i].timestamp
+		var point_time_delta := points[i + 1].timestamp - points[i].timestamp
 		if(point_time_delta == 0): point_time_delta = 1
-		var weight = distance(points[i + 1].pos, points[i].pos) / point_time_delta
+		var weight := distance(points[i + 1].pos, points[i].pos) / point_time_delta
 		sum += line_min_distance(points[i].pos, target_line) * weight
 		weight_sum += weight
 		#print("min_dist: %.f, distance: %.f, sum: %d, weighted_sum: %d " % [line_min_distance(points[i].pos, target_line),distance(points[i + 1].pos, points[i].pos), sum, weight_sum])
@@ -86,10 +86,10 @@ func calculate_score_straight_line(line_data: BrushStroke, target_line: BrushStr
 # calculate the min distance of the point to the target line considering bounds
 func line_min_distance(point: Vector2, line: BrushStroke) -> float:
 	# calculate the angle of the target line and the point to the start and end
-	var target_line_angle = calculate_angle(line.start, line.end) + PI / 2
+	var target_line_angle := calculate_angle(line.start, line.end) + PI / 2
 	
-	var pos1 = point_location(point, line.start, target_line_angle)
-	var pos2 = point_location(point, line.end, target_line_angle)
+	var pos1 := point_location(point, line.start, target_line_angle)
+	var pos2 := point_location(point, line.end, target_line_angle)
 	
 	if pos1 != pos2:
 		return point_to_line_distance(point, line.start, line.end)
@@ -98,14 +98,14 @@ func line_min_distance(point: Vector2, line: BrushStroke) -> float:
 
 func point_to_line_distance(point: Vector2, line_start: Vector2, line_end: Vector2) -> float:
 	# Line segment direction
-	var dx = line_end.x - line_start.x
-	var dy = line_end.y - line_start.y
+	var dx := line_end.x - line_start.x
+	var dy := line_end.y - line_start.y
 
 	# Numerator of the distance formula
 	var numerator = abs(dy * point.x - dx * point.y + line_end.x * line_start.y - line_end.y * line_start.x)
 	
 	# Denominator of the distance formula (line segment length)
-	var denominator = sqrt(dy * dy + dx * dx)
+	var denominator := sqrt(dy * dy + dx * dx)
 	
 	# Avoid division by zero
 	if denominator == 0:
@@ -118,12 +118,12 @@ func point_to_line_distance(point: Vector2, line_start: Vector2, line_end: Vecto
 # Determines if the point is on one side of the line defined by target_point and target_angle
 func point_location(point: Vector2, target_point: Vector2, target_angle: float) -> bool:
 	# Line equation components: ax + by + c = 0
-	var a = sin(target_angle)
-	var b = -cos(target_angle)
-	var c = -(a * target_point.x + b * target_point.y)
+	var a := sin(target_angle)
+	var b := -cos(target_angle)
+	var c := -(a * target_point.x + b * target_point.y)
 	
 	# Calculate the localization value for the point
-	var localization = a * point.x + b * point.y + c
+	var localization := a * point.x + b * point.y + c
 	
 	# Debugging print statements to understand the localization value
 	#print("Point: ", point)
@@ -146,9 +146,9 @@ func to_degrees(angle: float) -> float:
 # function to check if the line is completed sucefully 
 # for osu calculations i have no idea how to make this
 func has_line_completed(line_data: BrushStroke, target_line: BrushStroke, tolerance: float = 0.20) -> bool:
-	var distance_start_min = INF
-	var distance_end_min = INF
-	var target_dist = distance(target_line.start, target_line.end) * tolerance
+	var distance_start_min := INF
+	var distance_end_min := INF
+	var target_dist := distance(target_line.start, target_line.end) * tolerance
 	# calculate the distance of all points distances to the start and end of target line
 	for point in line_data.data:
 		distance_start_min = min(distance_start_min, distance(point.pos, target_line.start))
@@ -161,7 +161,7 @@ func has_line_completed(line_data: BrushStroke, target_line: BrushStroke, tolera
 func curve_min_distance(point: Vector2, curve_target: BrushStroke) -> float:
 	# its more simple because you dont have to check for bounds but its more expensive because you have to calculate the distance for every point
 
-	var min_distance = 9223372036854775807
+	var min_distance := 9223372036854775807
 	for i in range(curve_target.data.size() - 1):
 		min_distance = min(min_distance, distance(point, curve_target.data[i].pos))
 	return min_distance
